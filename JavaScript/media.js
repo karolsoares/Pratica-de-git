7e37d336fb758b68fe6169af76012342fd2fd437
karolsoares <86377137+karolsoares@users.noreply.github.com>
2021-09-25 13:33:31 -0300

    Create media.js

---
+++ JavaScript/media.js
@@ -0,0 +1,16 @@
+console.log("Programa para realizarmos o cálculo de nota média")
+
+var media1= 10
+var media2= 3
+
+var media_total= (media1 + media2) /2
+
+console.log("A média é:" + media_total)
+
+if(media_total > 6){
+    console.log("Aprovado")
+}
+else{
+    console.log("Reprovado")
+}
+
