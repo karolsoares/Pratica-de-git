df504020f527db3b5bafd6791d29c674b3a90312
karolsoares <86377137+karolsoares@users.noreply.github.com>
2021-09-25 13:33:54 -0300

    Create variaveis.js

---
+++ JavaScript/variaveis.js
@@ -0,0 +1,20 @@
+// var é para dclararms uma variavel
+var nome = "João" // string
+var age = 18 // inteiro
+var altura = 1.80 // float
+var vivo = true // bollean
+var alimento = null // null
+var time // indefinido
+
+var cesta = ["abacaxi", "bergamota", "maça", "uva"] // lista
+var misturada = ["abacaxi", 10, 11.9, true, "fim"] // lista com valores diferentes
+
+var pessoa = {
+    nome: "João",
+    age: 18,
+    altura: 1.80,
+    vivo: false,
+    time: "Corintia"
+} // obejto
+
+console.log(cesta[2])
